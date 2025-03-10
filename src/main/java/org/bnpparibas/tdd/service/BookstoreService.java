d4009c6b291eaf27e4ed31ed474b2f04bc48cc98
Prabhakaran Raju <rsprabhakara@gmail.com>
2025-03-10 14:22:49 +0530

    feat: added failed test case single book price calc

--- src/main/java/org/bnpparibas/tdd/service/BookstoreService.java
+++ src/main/java/org/bnpparibas/tdd/service/BookstoreService.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class BookstoreService {
 
     public int calculatePrice(List<Book> books) {
+        if (books.size() == 1) {
+            return 50;
+        }
         return 0;
     }
 }
