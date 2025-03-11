3ac2677b46c446b263f7aab25d93f11a23f38b08
Prabhakaran Raju <rsprabhakara@gmail.com>
2025-03-11 22:30:58 +0530

    Refactor: passed all tests with expected results

--- src/main/java/org/bnpparibas/tdd/service/BookstoreService.java
+++ src/main/java/org/bnpparibas/tdd/service/BookstoreService.java
@@ -2,51 +2,51 @@ package org.bnpparibas.tdd.service;
 
 import org.bnpparibas.tdd.model.Book;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BookstoreService {
 
     private static final double BOOK_PRICE = 50;
     private static final double[] DISCOUNTS = {0.0, 0.0, 0.05, 0.10, 0.20, 0.25};
+    private static final Map<String, Double> memo = new HashMap<>();
+    private int bookSize;
 
-    private double findMinPrice(List<Integer> bookCounts) {
-        if (bookCounts.isEmpty()) {
-            return 0.0;
+    public double findMinPrice(List<Integer> bookQuantities) {
+        bookQuantities.removeIf(q -> q == 0);
+        if (bookQuantities.isEmpty()) {
+            return 0.0;//base condition
+        }
+        String key = bookQuantities.toString();
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
         double minPrice = Double.MAX_VALUE;
-        for (int i = 1; i < bookCounts.size(); i++) {
-            List<Integer> remainingBooks = new ArrayList<>(bookCounts);
-            for (int j = 0; j < i; j++) {
-                bookCounts.set(j, remainingBooks.get(j) - 1);
+        for (int setSize = Math.min(bookQuantities.size(), bookSize); setSize >= 1; setSize--) {
+            List<Integer> remainingBooks = new ArrayList<>(bookQuantities);
+            //System.out.println("Outer : " + remainingBooks);
+            int count = 0;
+            for (int i = 0; i < remainingBooks.size() && count < setSize; i++) {
+                if (remainingBooks.get(i) > 0) {
+                    remainingBooks.set(i, remainingBooks.get(i) - 1);
+                    count++;
+                }
+                //System.out.println("Inner : " + remainingBooks);
             }
-            double price = i * BOOK_PRICE * (1 - DISCOUNTS[i]);
-            minPrice = Math.min(minPrice, price);
+            if (count == setSize) {
+                double price = (setSize * BOOK_PRICE * (1 - DISCOUNTS[setSize])) + findMinPrice(remainingBooks);
+                minPrice = Math.min(minPrice, price);
+            }
         }
+        memo.put(key, minPrice);
         return minPrice;
     }
 
+
     public double calculatePrice(Map<String, Integer> books) {
         List<Integer> bookCounts = new ArrayList<>(books.values());
+        bookSize = bookCounts.size();
         Collections.sort(bookCounts, Collections.reverseOrder());
         return findMinPrice(bookCounts);
-//        double totalPrice = 0.0;
-//        while (true) {  //to form groups
-//            int distinctBooks = 0;
-//            for (int i = 0; i < bookCounts.size(); i++) {
-//                if (bookCounts.get(i) > 0) {
-//                    bookCounts.set(i, bookCounts.get(i) - 1);
-//                    distinctBooks++;
-//                }
-//            }
-//            if (distinctBooks == 0) {
-//                break;
-//            }
-//            totalPrice += distinctBooks * BOOK_PRICE * (1 - DISCOUNTS[distinctBooks]);
-//        }
-//        return totalPrice;
     }
 
 }
